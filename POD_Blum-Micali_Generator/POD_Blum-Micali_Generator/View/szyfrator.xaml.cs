f2b28aed72b9bc32c0707c3a1617dc46e7e143c5
witkowski01 <witkowski01@wp.pl>
2014-12-01 23:46:51 +0100

    No to działa

--- POD_Blum-Micali_Generator/POD_Blum-Micali_Generator/View/szyfrator.xaml.cs
+++ POD_Blum-Micali_Generator/POD_Blum-Micali_Generator/View/szyfrator.xaml.cs
@@ -27,51 +27,37 @@ namespace POD_Blum_Micali_Generator.View
         {
             InitializeComponent();
         }
-        const int bits = 16;
-        string in_s, gen_s, out_s;
-        bool[] in_b, out_b;
-        bool krok1, krok2, krok3;
-        
-        private void krokowyClick(object sender, RoutedEventArgs e)
-        {
-            krok1 = true;
-            krok2 = true;
-            krok3 = true;
-        }
 
+        private string wej_string, klucz_string, wyj_string;
+        private bool[] wej_bool, wyj_bool;
+        private const int bity = 16;
+       
         private void Szyfruj(object sender, RoutedEventArgs e)
         {
             try
             {
-                in_s = System.IO.File.ReadAllText(ZamienPola.IsChecked == false? wej.Text:wyj.Text, Encoding.UTF8);
-                gen_s = System.IO.File.ReadAllText(klucz.Text);
-                out_s = "";
-                in_b = new bool[in_s.Length * bits];
-                out_b = new bool[in_b.Length];
-                ///
-                if (Krokowy.IsChecked == true)
-                {
-                    MessageBoxResult wynik = MessageBox.Show("Tryb prezentacji krokowej"  + "\n\nTekst przed procesem:\n" + in_s, "Tryb prezentacji krokowej");
-                    if (wynik == MessageBoxResult.Cancel) Krokowy.IsChecked = false;
-                }
-                ///
+                wej_string = System.IO.File.ReadAllText(ZamienPola.IsChecked == false? wej.Text:wyj.Text);
+                klucz_string = System.IO.File.ReadAllText(klucz.Text);
+                
+                wej_bool = new bool[wej_string.Length * bity];
+                wyj_bool = new bool[wej_bool.Length];
 
-                for(int i = 0; i< in_s.Length; i++)
+                for(var i = 0; i< wej_string.Length; i++)
                 {
-                    in_b = getBool(in_s[i], i, in_b);       //Zamiana char na bool[]
+                    wej_bool = getBool(wej_string[i], i, wej_bool);       //Zamiana char na bool[]
                 }
 
+                xor();     //Xorowanie bool[] klucza i pliku wejściowego
 
-                xor();                                      //Xorowanie bool[] klucza i pliku wejściowego
-
-                for (int i = 0; i < in_s.Length; i++)
+                for (var i = 0; i < wej_string.Length; i++)
                 {
-                    out_s += getChar(out_b, i);             //Zamiana bool[] na char
+                    wyj_string += getChar(wyj_bool, i);      //Zamiana bool[] na char
                 }
 
-                System.IO.File.WriteAllText(ZamienPola.IsChecked == false ? wyj.Text : wej.Text, out_s);
-                Krokowy.IsChecked = false;
-                MessageBox.Show("Gotowe!\nZakodowana wiadomość:\n" + out_s ,"Kodowanie zakończone",
+                
+                System.IO.File.WriteAllText(ZamienPola.IsChecked == false ? wyj.Text : wej.Text, wyj_string);  //nie wiem dlaczego tylko 58 znaków koduje
+               
+                MessageBox.Show("Koniec.\nZaszyfrowana wiadomość:\n" + wyj_string ,"Szyfrowanie zakończone",
                     MessageBoxButton.OK, MessageBoxImage.Asterisk);
             }
             catch (Exception err)
@@ -83,112 +69,55 @@ namespace POD_Blum_Micali_Generator.View
         private bool[] getBool(char znak, int k, bool[] tablica)
         {
             int nr = Convert.ToInt32(znak);
-            for (int i = 0; i < bits; i++)
+            for (var i = 0; i < bity; i++)
             {
-                if (nr >= Math.Pow(2, (bits -1) - i))
+                if (nr >= Math.Pow(2, (bity -1) - i))
                 {
-                    tablica[i + bits*k] = true;
-                    nr = nr - (int)Math.Pow(2, (bits - 1) - i);
+                    tablica[i + bity*k] = true;
+                    nr = nr - (int)Math.Pow(2, (bity - 1) - i);
                 }
                 else
                 {
-                    tablica[i + bits * k] = false;
+                    tablica[i + bity * k] = false;
                 }
             }
-            ///
-            if (Krokowy.IsChecked == true && krok1 == true)
-            {
-                string mess = "Rozkład znaku nr: " + (k + 1) + "\nZnak wejściowy: " + znak + "\nNumer znaku w UTF: " +
-                    Convert.ToInt32(znak) + "\n\nWyjściowy kod binarny:\n";
-                for (int j = 0; j < bits; j++)
-                {
-                    if (bits - j < 10)
-                        mess += 0;
-                    mess += bits - j + " ";
-                }
-                mess += "    Potęgi 2\n ";
-                for (int i = 0; i < bits; i++)
-                {
-                    mess += tablica[bits * k + i] ? "1" : "0";
-                    mess += "   ";
-                }
-                mess += "\n\nNastępny znak?";
-                wynik = MessageBox.Show(mess, "Tryb prezentacji krokowej", MessageBoxButton.YesNoCancel, 
-                    MessageBoxImage.Information);
 
-                if (wynik == MessageBoxResult.Cancel) Krokowy.IsChecked = false;
-                if (wynik == MessageBoxResult.No) krok1 = false;
-            }
-            ///
             return tablica;
         }
 
-        private MessageBoxResult wynik;
         private void xor()
         {
-            int j = 0;
+            var j = 0;
 
-            foreach (bool element in in_b)              
+            foreach (var element in wej_bool)              
             {
-                if (gen_s.Length < j)
+                        if (klucz_string.Length < j)
+                        {
+                            MessageBox.Show("Za długi tekst lub \nZa krótki wygenerowany ciąg",
+                            " To błąd krytyczny", MessageBoxButton.OK, MessageBoxImage.Error);
+                            break;
+                        }
+
+                if ((wej_bool[j] == false && klucz_string[j] == '0') || (wej_bool[j] == true && klucz_string[j] == '1'))
                 {
-                    MessageBox.Show("Za długi tekst\nZa krótki wygenerowany ciąg", "Błąd krytyczny", MessageBoxButton.OK, MessageBoxImage.Error);
-                    break;
-                }
-                if ((in_b[j] == false && gen_s[j] == '0') || (in_b[j] == true && gen_s[j] == '1'))
-                {
-                    out_b[j] = false;
+                    wyj_bool[j] = false;
                 }
                 else
                 {
-                    out_b[j] = true;
+                    wyj_bool[j] = true;
                 }
-                ///
-                if (Krokowy.IsChecked == true && krok2 == true)
-                {
-                    string mess = "Numer bitu: "+ (j+1) +"\nXORowanie tekstu z wygenerowanym ciagiem\n\nBit tekstu: ";
-                    mess += in_b[j] ? "1" : "0";
-                    mess += "\nBit z generatora: " + gen_s[j] + "\nBit wynikowy: ";
-                    mess += out_b[j] ? "1" : "0";
-                    wynik = MessageBox.Show(mess, "Tryb prezentacji krokowej", MessageBoxButton.YesNoCancel, MessageBoxImage.Information);
-                    if (wynik == MessageBoxResult.Cancel) Krokowy.IsChecked = false;
-                    if (wynik == MessageBoxResult.No) krok2 = false;
-                }
-                ///
                 j++;
             }  
         }
         private string getChar(bool[] tablica, int k){
-            int znak = 0;
-            for (int i = 0; i < bits; i++)
+            var znak = 0;
+            for (var i = 0; i < bity; i++)
             {
-                if (tablica[i + k*bits])
+                if (tablica[i + k*bity])
                 {
-                    znak += (int)Math.Pow(2, (bits - 1) - i);
+                    znak += (int)Math.Pow(2, (bity - 1) - i);
                 }
             }
-            ///
-            if (Krokowy.IsChecked == true && krok3 == true)
-            {
-                string mess = "Składanie znaku nr: " + (k + 1) + "\nBity wejściowe:\n";
-                for (int j = 0; j < bits; j++)
-                {
-                    if (bits - j < 10)
-                        mess += 0;
-                    mess += bits - j + " ";
-                }
-                mess += "    Potęgi 2\n ";
-                for (int j = 0; j < bits; j++)
-                {
-                    mess += tablica[bits * k + j] ? "1" : "0";
-                    mess += "   ";
-                }
-                mess += "\nNumer znaku w UTF: " + (int)znak + "\nZnak wyjściowy: " + Convert.ToChar(znak).ToString() + "\n\nNastępny znak?";
-                wynik = MessageBox.Show(mess, "Tryb prezentacji krokowej", MessageBoxButton.YesNoCancel, MessageBoxImage.Information);
-                if (wynik == MessageBoxResult.Cancel) Krokowy.IsChecked = false;
-                if (wynik == MessageBoxResult.No) krok3 = false;
-            }
-            ///
             return Convert.ToChar(znak).ToString();
         }
 
