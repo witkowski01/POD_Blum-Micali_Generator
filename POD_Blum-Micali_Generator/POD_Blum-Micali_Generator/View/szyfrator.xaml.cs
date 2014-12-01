96bc02a8d19f0e9f83b40c21ae0239aea0630969
witkowski01 <witkowski01@wp.pl>
2014-12-01 23:04:24 +0100

    Dodanie kroków

--- POD_Blum-Micali_Generator/POD_Blum-Micali_Generator/View/szyfrator.xaml.cs
+++ POD_Blum-Micali_Generator/POD_Blum-Micali_Generator/View/szyfrator.xaml.cs
@@ -48,7 +48,13 @@ namespace POD_Blum_Micali_Generator.View
                 out_s = "";
                 in_b = new bool[in_s.Length * bits];
                 out_b = new bool[in_b.Length];
-
+                ///
+                if (Krokowy.IsChecked == true)
+                {
+                    MessageBoxResult wynik = MessageBox.Show("Tryb prezentacji krokowej"  + "\n\nTekst przed procesem:\n" + in_s, "Tryb prezentacji krokowej");
+                    if (wynik == MessageBoxResult.Cancel) Krokowy.IsChecked = false;
+                }
+                ///
 
                 for(int i = 0; i< in_s.Length; i++)
                 {
@@ -65,7 +71,7 @@ namespace POD_Blum_Micali_Generator.View
 
                 System.IO.File.WriteAllText(ZamienPola.IsChecked == false ? wyj.Text : wej.Text, out_s);
                 Krokowy.IsChecked = false;
-                MessageBox.Show("Gotowe!\nZakodowana wiadomość:\n" + out_s, "Kodowanie wykonane bardzo dobrze",
+                MessageBox.Show("Gotowe!\nZakodowana wiadomość:\n" + out_s ,"Kodowanie zakończone",
                     MessageBoxButton.OK, MessageBoxImage.Asterisk);
             }
             catch (Exception err)
@@ -89,9 +95,35 @@ namespace POD_Blum_Micali_Generator.View
                     tablica[i + bits * k] = false;
                 }
             }
+            ///
+            if (Krokowy.IsChecked == true && krok1 == true)
+            {
+                string mess = "Rozkład znaku nr: " + (k + 1) + "\nZnak wejściowy: " + znak + "\nNumer znaku w UTF: " +
+                    Convert.ToInt32(znak) + "\n\nWyjściowy kod binarny:\n";
+                for (int j = 0; j < bits; j++)
+                {
+                    if (bits - j < 10)
+                        mess += 0;
+                    mess += bits - j + " ";
+                }
+                mess += "    Potęgi 2\n ";
+                for (int i = 0; i < bits; i++)
+                {
+                    mess += tablica[bits * k + i] ? "1" : "0";
+                    mess += "   ";
+                }
+                mess += "\n\nNastępny znak?";
+                wynik = MessageBox.Show(mess, "Tryb prezentacji krokowej", MessageBoxButton.YesNoCancel, 
+                    MessageBoxImage.Information);
 
+                if (wynik == MessageBoxResult.Cancel) Krokowy.IsChecked = false;
+                if (wynik == MessageBoxResult.No) krok1 = false;
+            }
+            ///
             return tablica;
         }
+
+        private MessageBoxResult wynik;
         private void xor()
         {
             int j = 0;
@@ -111,7 +143,18 @@ namespace POD_Blum_Micali_Generator.View
                 {
                     out_b[j] = true;
                 }
-
+                ///
+                if (Krokowy.IsChecked == true && krok2 == true)
+                {
+                    string mess = "Numer bitu: "+ (j+1) +"\nXORowanie tekstu z wygenerowanym ciagiem\n\nBit tekstu: ";
+                    mess += in_b[j] ? "1" : "0";
+                    mess += "\nBit z generatora: " + gen_s[j] + "\nBit wynikowy: ";
+                    mess += out_b[j] ? "1" : "0";
+                    wynik = MessageBox.Show(mess, "Tryb prezentacji krokowej", MessageBoxButton.YesNoCancel, MessageBoxImage.Information);
+                    if (wynik == MessageBoxResult.Cancel) Krokowy.IsChecked = false;
+                    if (wynik == MessageBoxResult.No) krok2 = false;
+                }
+                ///
                 j++;
             }  
         }
@@ -124,7 +167,28 @@ namespace POD_Blum_Micali_Generator.View
                     znak += (int)Math.Pow(2, (bits - 1) - i);
                 }
             }
-
+            ///
+            if (Krokowy.IsChecked == true && krok3 == true)
+            {
+                string mess = "Składanie znaku nr: " + (k + 1) + "\nBity wejściowe:\n";
+                for (int j = 0; j < bits; j++)
+                {
+                    if (bits - j < 10)
+                        mess += 0;
+                    mess += bits - j + " ";
+                }
+                mess += "    Potęgi 2\n ";
+                for (int j = 0; j < bits; j++)
+                {
+                    mess += tablica[bits * k + j] ? "1" : "0";
+                    mess += "   ";
+                }
+                mess += "\nNumer znaku w UTF: " + (int)znak + "\nZnak wyjściowy: " + Convert.ToChar(znak).ToString() + "\n\nNastępny znak?";
+                wynik = MessageBox.Show(mess, "Tryb prezentacji krokowej", MessageBoxButton.YesNoCancel, MessageBoxImage.Information);
+                if (wynik == MessageBoxResult.Cancel) Krokowy.IsChecked = false;
+                if (wynik == MessageBoxResult.No) krok3 = false;
+            }
+            ///
             return Convert.ToChar(znak).ToString();
         }
 
